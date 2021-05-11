d25db3a4d8a384151db15841869df4c9422e8a5d
高津亘宏 <nobuhiro19980622@gmail.com>
2021-05-11 15:52:17 +0900

    refactor line.js　リファクタリング

--- line.js
+++ line.js
@@ -1,31 +1,51 @@
-//Linebot
-function line() {
-  const url = "https://api.line.me/v2/bot/message/push"
-  const message = "test"
-  const postData = {
-    "to": PropertiesService.getScriptProperties().getProperty("USER_ID"), 
-    "messages": [
-      {
-        "type": "text",
-        "text": message,
-      }
-    ]
-  };
+class line {
+  constructor(id){
+    this.id = PropertiesService.getScriptProperties().getProperty(id)
+    this.headers = {
+      "Content-Type": "application/json; charset=UTF-8",
+      'Authorization': 'Bearer ' + PropertiesService.getScriptProperties().getProperty("LINE_TOKEN"), 
+    };
+  }
 
-  const headers = {
-    "Content-Type": "application/json; charset=UTF-8",
-    'Authorization': 'Bearer ' + PropertiesService.getScriptProperties().getProperty("LINE_TOKEN"), 
-  };
+  post_message(url,postData){
+    const options = {
+      "method": "post",
+      "headers": this.headers,
+      "payload": JSON.stringify(postData)
+    };
 
-  const options = {
-    "method": "post",
-    "headers": headers,
-    "payload": JSON.stringify(postData)
-  };
+    UrlFetchApp.fetch(url, options);
+  }
 
-  UrlFetchApp.fetch(url, options);
+  push(message) {
+    const url = "https://api.line.me/v2/bot/message/push"
+    const postData = {
+      "to": this.id, 
+      "messages": [
+        {
+          "type": "text",
+          "text": message,
+        }
+      ]
+    };
+    this.post_message(url,postData)
+  }
 
-
-
+  broadCast(message){
+    const url = "https://api.line.me/v2/bot/message/broadcast"
+    const postData = {
+      "messages": [
+        {
+          "type": "text",
+          "text": message,
+        }
+      ]
+    };
+    this.post_message(url,postData)
+  }
 }
 
+function a_test(){
+  const test_line = new line()
+  test_line.broadCast("oppai")
+}
