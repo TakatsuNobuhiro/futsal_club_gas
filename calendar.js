469509421e77d345268a26755a838dae50932198
高津亘宏 <nobuhiro19980622@gmail.com>
2021-05-11 15:00:57 +0900

    refactor calendar リファクタリング

--- calendar.js
+++ calendar.js
@@ -1,23 +1,37 @@
-
-function fetchEvents() {
-  let tomorrow = new Date();
-  tomorrow.setDate( tomorrow.getDate() + 1 );
-  const myCalendar = CalendarApp.getCalendarById(PropertiesService.getScriptProperties().getProperty("CALENDAR_ID")); //特定のIDのカレンダーを取得
-  const myEvents = myCalendar.getEventsForDay(tomorrow);　//カレンダーの本日のイベントを取得
-  let messages_list = []
-  /* イベントの数だけ繰り返し */
-  for(let i=0;i<myEvents.length;i++){
-    let strTitle=myEvents[i].getTitle(); //イベントのタイトル
-    let strStart=_HHmm(myEvents[i].getStartTime()); //イベントの開始時刻
-    let strEnd=_HHmm(myEvents[i].getEndTime()); //イベントの終了時刻
-    let strBody = '明日は' + strStart + 'から' + strEnd +'まで'+ strTitle + 'があります。'; //チャットワークに送る文字列にイベント内容を追加
-    messages_list.unshift(strBody)
+class calendar {
+  constructor(idProperty){
+    this.calendarId = PropertiesService.getScriptProperties().getProperty(idProperty)
   }
 
-  return messages_list
+  _HHmm(str){
+    return Utilities.formatDate(str,'JST','HH:mm')
+  };
+  fetchEvents() {
+    let tomorrow = new Date();
+    tomorrow.setDate( tomorrow.getDate() + 1 );
+    const myCalendar = CalendarApp.getCalendarById(this.calendarId); //特定のIDのカレンダーを取得
+    const tomorrow_events = myCalendar.getEventsForDay(tomorrow);　//カレンダーの本日のイベントを取得
+    Logger.log(tomorrow_events)
+    let events_list = []
+    /* イベントの数だけ繰り返し */
+    for(const event of tomorrow_events){
+      let title = event.getTitle(); //イベントのタイトル
+      let start = this._HHmm(event.getStartTime()); //イベントの開始時刻
+      let end = this._HHmm(event.getEndTime()); //イベントの終了時刻
+      let location = event.getLocation()
+      Logger.log(start)
 
 
-}
-function _HHmm(str){
-  return Utilities.formatDate(str,'JST','HH:mm')
+    };
+
+    return events_list
+
+
+  };
+
+};
+
+function test (){
+  const sample = new calendar("MY_CALENDAR_ID");
+  sample.fetchEvents()
 }
