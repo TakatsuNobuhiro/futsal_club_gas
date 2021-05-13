9a8685d67cfacf543ca72ccfaa3fc53fa5b6dc1f
高津亘宏 <nobuhiro19980622@gmail.com>
2021-05-14 00:00:20 +0900

    refactor calendar.js

--- calendar.js
+++ calendar.js
@@ -6,31 +6,25 @@ class calendar {
   changeDate(str){
     return Utilities.formatDate(str,'JST','HH:mm')
   };
-  fetchEvents() {
-    let tomorrow = new Date();
-    tomorrow.setDate( tomorrow.getDate() + 1 );
-    const myCalendar = CalendarApp.getCalendarById(this.calendarId); //特定のIDのカレンダーを取得
-    const tomorrow_events = myCalendar.getEventsForDay(tomorrow);　//カレンダーの本日のイベントを取得
-    let events_list = []
-    /* イベントの数だけ繰り返し */
-    for(const event of tomorrow_events){
-      let title = event.getTitle(); //イベントのタイトル
-      let start = this.changeDate(event.getStartTime()); //イベントの開始時刻
-      let end = this.changeDate(event.getEndTime()); //イベントの終了時刻
-      let location = event.getLocation()
-      Logger.log(start)
 
-
-    };
-
-    return events_list
-
-
+  fetchEvents(day) {
+    const myCalendar = CalendarApp.getCalendarById(this.calendarId); 
+    const events = myCalendar.getEventsForDay(day);　
+    return events
   };
 
 };
 
 function test (){
   const sample = new calendar("MY_CALENDAR_ID");
-  sample.fetchEvents()
+  let tomorrow = new Date();
+  tomorrow.setDate( tomorrow.getDate() + 1 );
+  tomorrow_events = sample.fetchEvents(tomorrow)
+  for(const event of tomorrow_events){
+    let title = event.getTitle(); 
+    let start = this.changeDate(event.getStartTime()); 
+    let end = this.changeDate(event.getEndTime()); 
+    let location = event.getLocation()
+
+  };
 }
